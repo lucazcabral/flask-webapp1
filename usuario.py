c76421393f1293b5e05a4d86b5cec60c314cb923
Lucas Silva Cabral <lucaz.cabral@gmail.com>
2020-06-18 21:13:30 -0300

    Finalização de curso

--- usuario.py
+++ usuario.py
@@ -16,41 +16,33 @@ class Usuario:
                f'{self.__login};' \
                f'{self.__senha};'
 
-
     @property
     def id(self) -> int:
         return self.__id
-
 
     @id.setter
     def id(self, valor: int) -> None:
         self.__id = valor
 
-
     @property
     def nome(self) -> str:
         return self.__nome
-
 
     @nome.setter
     def nome(self, valor: str) -> None:
         self.__nome = valor
 
-
     @property
     def login(self) -> str:
         return self.__login
-
 
     @login.setter
     def login(self, valor: str) -> None:
         self.__login = valor
 
-
     @property
     def senha(self) -> str:
         return self.__senha
-
 
     @senha.setter
     def senha(self, valor: str) -> None:
